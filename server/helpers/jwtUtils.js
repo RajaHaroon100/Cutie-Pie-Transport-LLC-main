550c47bfd69cafd214583202bec8e10004aa4536
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-09 18:59:05 +0500

    Update jwtUtils.js

--- server/helpers/jwtUtils.js
+++ server/helpers/jwtUtils.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const secretKey = process.env.SECRET_KEY;
+const secretKey = process.env.SECRET_KEY
 
 const generateToken = (user) => {
     return jwt.sign({ id: user._id, username: user.username }, secretKey, { expiresIn: '1h' });
 };
 
 const verifyToken = (token) => {
-    return new Promise((resolve, reject) => {
-        jwt.verify(token, secretKey, (err, decoded) => {
-            if (err) {
-                reject(err);
+ return new Promise((resolve, reject) => {
+     jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+        reject(err);
             } else {
                 resolve(decoded);
             }
@@ -20,3 +20,4 @@ const verifyToken = (token) => {
 };
 
 module.exports = { generateToken, verifyToken };
+
