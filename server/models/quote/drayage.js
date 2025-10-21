90d8dd83bb6428eafa7f0f6977b60d4139db18e6
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-22 02:16:48 +0500

    Update drayage.js

--- server/models/quote/drayage.js
+++ server/models/quote/drayage.js
@@ -1,1 +1,39 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
+const drayageQuoteSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    email: String,
+    equipmentSize: String,
+    weight: String,
+    agree: Boolean,
+    title: String,
+    company: String,
+    origin: String,
+    destination: String,
+    loadingDate: String,
+    deliveryDate: String,
+    description: String,
+    containersNumber: String,
+    commodity: String,
+    equipmentType: 
+    { 
+        type: String, 
+    },
+    status: 
+    { 
+        type: String, 
+        default: 'pending' 
+    },
+    requestedOn: 
+    { 
+        type: Date, 
+        default: Date.now 
+    },
+})
+
+const DrayageQuoteModel = mongoose.model('DrayageQuote', drayageQuoteSchema);
+
+module.exports = DrayageQuoteModel;
