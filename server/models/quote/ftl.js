a5babf7c7557e887b0bc64ea67916064fe26c2af
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-22 02:24:25 +0500

    Update ftl.js

--- server/models/quote/ftl.js
+++ server/models/quote/ftl.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
+const ftlQuoteSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    email: String,
+    trailerSize: String,
+    weight: String,
+    agree: Boolean,
+    title: String,
+    company: String,
+    origin: String,
+    destination: String,
+    loadingDate: String,
+    deliveryDate: String,
+    description: String,
+    equipment: 
+    { 
+        type: String, 
+    },
+    status: 
+    { 
+        type: String, 
+        default: 'pending' 
+    },
+    requestedOn: 
+    { 
+        type: Date, 
+        default: Date.now 
+    },
+})
+
+const FTLQuoteModel = mongoose.model('FTLQuote', ftlQuoteSchema);
+
+module.exports = FTLQuoteModel;
