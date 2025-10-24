2a4c4b97e6be5c69e65f310043e897b3e95c1f35
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-25 02:01:54 +0500

    Update Checkpoint.js

--- server/models/Checkpoint.js
+++ server/models/Checkpoint.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
+
+const CheckpointSchema = new mongoose.Schema({
+    orderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Order'
+    },
+    trackingId: {
+        type: String,
+        required: true
+    },
+    checkpoints: [{
+        location: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: Boolean,
+            default: false
+        }
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Checkpoint', CheckpointSchema); 
+
+
 
