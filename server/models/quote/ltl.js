2bdd762353e41c3ebd1755fb49dd7d31929a1a69
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-27 23:02:02 +0500

    Create LTLQuote model with schema definition

--- server/models/quote/ltl.js
+++ server/models/quote/ltl.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
+const ltlQuoteSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    email: String,
+    length: String,
+    weight: String,
+    agree: Boolean,
+    title: String,
+    company: String,
+    origin: String,
+    destination: String,
+    loadingDate: String,
+    deliveryDate: String,
+    description: String,
+    equipment: 
+    { 
+        type: String, 
+    },
+    status: 
+    { 
+        type: String, 
+        default: 'pending' 
+    },
+    requestedOn: 
+    { 
+        type: Date, 
+        default: Date.now 
+    },
+})
+
+const LTLQuoteModel = mongoose.model('LTLQuote', ltlQuoteSchema);
+
+module.exports = LTLQuoteModel;
