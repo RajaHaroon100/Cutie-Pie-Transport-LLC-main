8c1b4eb1667dda7f8e9c8a23f15a22e251cb14ce
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-30 00:49:02 +0500

    Add QuoteCard component with loading date formatting

--- client/src/Admin/pages/Panel/Quote/QuoteCard.jsx
+++ client/src/Admin/pages/Panel/Quote/QuoteCard.jsx
@@ -1,1 +1,17 @@
+import moment from "moment";
 
+export default function QuoteCard({ onClick, firstName, lastName, loadingDate, type }) {
+    return(
+        <div onClick={onClick} className="max-w-sm rounded overflow-hidden shadow-sm p-4 border-b font-sans text-gray-900 border-gray-400 bg-gray-100 hover:bg-gray-300">
+            <div className="font-bold text-xl mb-1">{firstName} {lastName}</div>
+            <p className="text-gray-700 text-sm">
+                Loading Date: 
+                <span className="font-semibold text-base"> {moment(loadingDate).format('DD-MM-YYYY')}</span>
+            </p>
+            <p className="text-gray-700 text-sm">
+                Type: 
+                <span className="font-semibold text-base"> {type}</span>
+            </p>
+        </div>
+    );
+}
