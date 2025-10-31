f2c8a03605da9706cdf130e05ad6d3e3e15da831
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-31 23:10:36 +0500

    Update headBanner.jsx

--- client/src/components/headBanner.jsx
+++ client/src/components/headBanner.jsx
@@ -1,1 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function HeadBanner() {
+    return (
+        <>
+            <div className="bg-secondary text-white py-2 px-4 md:px-20 lg:px-44 shadow-md font-sans">
+                <div className="flex justify-end space-x-4 text-sm md:text-base">
+                    <Link to="/quote" className="hover:underline">Get A Quote</Link>
+                    <p>|</p>
+                    <Link to="/trackShipment" className=" hover:underline">Track Shipment</Link>
+                    <p>|</p>
+                    <Link to="/login" className="hover:underline">Admin LogIn</Link>
+                </div>
+            </div>
+        </>
+    );
+}
 
