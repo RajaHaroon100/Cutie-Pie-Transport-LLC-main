d8991b1cbafe2ff6355ba58fef92a1a80d16735d
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-11-02 00:18:15 +0500

    Update ScrollToTop.jsx

--- client/src/components/Essentials/ScrollToTop.jsx
+++ client/src/components/Essentials/ScrollToTop.jsx
@@ -1,14 +1,1 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = ({ children }) => {
-    const { pathname } = useLocation();
-
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
-
-    return children;
-};
-
-export default ScrollToTop;
