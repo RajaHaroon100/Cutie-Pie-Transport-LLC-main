e2311c3aa5210327a03f13ad6b380445e1bf47bf
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-11-06 01:04:53 +0500

    Update AboutUs.jsx

--- client/src/pages/About Us/AboutUs.jsx
+++ client/src/pages/About Us/AboutUs.jsx
@@ -1,1 +1,22 @@
+import Landing from "../../components/landing"
+import AboutUs from "../Content/AboutUs"
+import AboutCompany from "../Content/AboutCompany"
+import AboutServices from "../Content/AboutServices"
+import Values from "./Values"
+import AboutCEO from "./AboutCEO"
+import Testimonial from "../Content/Testimonial"
+
+export default function aboutUs() {
+    return (
+        <div className="bg-bg pb-12">
+            <Landing title="About Us" />
+            <AboutCEO />
+            <AboutUs />
+            <Values />
+            <AboutServices />
+            <AboutCompany />
+            <Testimonial />
+        </div>
+    );
+}
 
