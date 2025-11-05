58531a30db33f56b6854870690aa11d0f7e20037
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-11-06 01:08:12 +0500

    Update ContactUs.jsx

--- client/src/pages/Contact Us/ContactUs.jsx
+++ client/src/pages/Contact Us/ContactUs.jsx
@@ -1,17 +1,1 @@
-import Landing from "../../components/landing"
-import ContactInfo from "./ContactInfo"
-import BookLoad from "./BookLoad"
-import Message from "./ContactForm"
-import MapLocation from "../Content/MapLocation"
 
-export default function ContactUs() {
-    return (
-        <div className="bg-bg pb-24">
-            <Landing title="Contact Us"/>
-            <ContactInfo />
-            <BookLoad />
-            <Message />
-            <MapLocation />
-        </div>
-    );
-}
