e97c9faf2a8b655c572475071255d3eafad2b55b
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-11-09 23:27:21 +0500

    Update ServicesMapper.jsx

--- client/src/pages/Content/ServicesMapper.jsx
+++ client/src/pages/Content/ServicesMapper.jsx
@@ -1,1 +1,17 @@
+import Card from './Card.jsx'
 
+import { SERVICES } from './data.js'
+
+export default function Services() {
+    return(
+        <div className='font-sans pt-8' data-aos="fade-up">
+            <div className='text-center'>
+                <h1 className='text-3xl md:text-4xl font-bold text-heading_2 mt-2 md:mt-4 mb-6'>Our Services</h1>
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 px-6 md:px-24 lg:px-36' data-aos="fade-up">
+                {SERVICES.map((service, itr) => (<Card key={itr} mapper="services" {...service}/>))}
+            </div>
+        </div>
+    );
+}
