0fc17e18e940a5682335e77e76bfc9af3b0e0058
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-11-11 22:43:50 +0500

    Update Order.jsx

--- client/src/pages/Order/Order.jsx
+++ client/src/pages/Order/Order.jsx
@@ -1,1 +1,13 @@
+import Landing from "../../components/landing";
+import PlaceOrderForm from "./PlaceOrderForm";
 
+export default function Order() {
+    return(
+        <div className="bg-bg pb-24">
+            <Landing title='Place Order'/>
+            <div className="flex justify-center pt-24">
+                <PlaceOrderForm />
+            </div>
+        </div>
+    );
+}
