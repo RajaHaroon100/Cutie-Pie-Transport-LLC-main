5aaa08816dba13e50a0d2dabcaad3e0f052ff6d4
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-11-11 22:44:21 +0500

    Update PlaceOrder.jsx

--- client/src/pages/Order/PlaceOrder.jsx
+++ client/src/pages/Order/PlaceOrder.jsx
@@ -1,13 +1,1 @@
-import { useLocation } from 'react-router-dom';
 
-export default function PlaceOrder() {
-    
-    const location = useLocation();
-    const formData = location.state;
-    
-    return(
-        <div>
-            <h2>{formData}</h2>
-        </div>
-    );
-}
